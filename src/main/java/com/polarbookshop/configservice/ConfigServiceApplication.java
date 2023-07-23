9d9102de2e4f8eb61e9b2fbd9d01d6c7efb43da6
Howard Egana <howard.cu@gmail.com>
2023-07-23 17:19:24 -0400

    ch7- Package refactoring

--- src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
+++ src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package om.polarbookshop.configservice;
+package com.polarbookshop.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class PbsConfigServiceApplication {
+public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PbsConfigServiceApplication.class, args);
+		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
 }
